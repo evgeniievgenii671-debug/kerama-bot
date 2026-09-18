b56feee3836bd266ee7c287a26f9d132fa7aa619
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-19 02:52:22 +0600

    Update gemini.py

--- services/gemini.py
+++ services/gemini.py
@@ -1,14 +1,12 @@
 import os
-from google import genai
+import google.generativeai as genai
 
 def ask_gemini(prompt: str) -> str:
     api_key = os.getenv("GEMINI_API_KEY")
     if not api_key:
         return "Ошибка: Не задан API ключ Gemini."
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt,
-    )
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
     return response.text
