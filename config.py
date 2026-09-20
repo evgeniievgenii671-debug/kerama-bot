cb845eac5186c39fc7f01a6dd238859875a8e98a
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-20 11:18:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
 MANAGER_CHAT_ID = int(os.environ.get("MANAGER_CHAT_ID", "0"))
-
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
 # === ������ Groq ===
 MODEL_MAIN = "openai/gpt-oss-120b"
 MODEL_BACKUP = "openai/gpt-oss-20b"
